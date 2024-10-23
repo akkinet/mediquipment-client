0b9a65ae9172aa4e0049bc10e6c8e52667704ce7
akkinet <akashsharma.zak@gmail.com>
2024-10-23 10:13:35 +0530

    user order info

---
+++ app/api/order/[user]/route.js
@@ -0,0 +1,27 @@
+import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { ddbDocClient } from "../../../../config/ddbDocClient";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, ctx) => {
+  try {
+    const email = ctx.params.user;
+    const params = {
+      TableName: "Orders",
+      IndexName: "email-index",
+      KeyConditionExpression: "customer_email = :email",
+      ExpressionAttributeValues: {
+        ":email": email,
+      },
+    };
+    const command = new QueryCommand(params);
+    const result = await ddbDocClient.send(command);
+    let orderList = [];
+    if(result.Items.length > 0)
+      orderList = result.Items;
+    
+    return NextResponse.json(orderList, {status: 200})
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
